b03df3fb71b770cd0e932633223d02c30816011a
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-03-21 00:52:27 +0100

    Add Model User_Model
    Adding new model that describes a user.

---
+++ WpfApp/Models/User_Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp.Models
+{
+    public class User_Model
+    {
+        public uint Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime TimestampCreated { get; set; }
+        public DateTime TimestampLastLogin { get; set; }
+    }
+}
